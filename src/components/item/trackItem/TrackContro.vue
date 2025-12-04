8b6d572b9465055d29a11eef947cd2d8e154b8e5
PlumesBook <hsu.cbeing@gmail.com>
2025-12-04 16:38:20 +0800

    feat: 调整分割功能；优化 TrackControl.vue 中图标区域的 HTML 结构和样式

--- src/components/item/trackItem/TrackContro.vue
+++ src/components/item/trackItem/TrackContro.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="pl-4 pb-1 pr-10 w-full h-10 border-b dark:border-gray-600 border-gray-300">
-    <div
-      class="float-left h-9 w-32 flex flex-row flex-nowrap items-center justify-around"
-    >
+    <div class="float-left h-9 w-32 flex flex-row flex-nowrap items-center justify-around">
       <div v-for="item of icons" :key="item.title" @click="handlerIcon(item)">
-        <el-tooltip
-          :disabled="item.disable"
-          class="bg-gray-400"
-          :effect="store.isDark ? 'dark' : 'light'"
-          :content="item.title"
-          placement="bottom-start"
-        >
-          <component
-            :is="item.icon"
-            class="focus:outline-0"
-            :class="item.disable ? 'cursor-not-allowed text-gray-400' : ''"
-          />
+        <el-tooltip :disabled="item.disable" class="bg-gray-400" :effect="store.isDark ? 'dark' : 'light'"
+          :content="item.title" placement="bottom-start">
+          <component :is="item.icon" class="focus:outline-0"
+            :class="item.disable ? 'cursor-not-allowed text-gray-400' : ''" />
         </el-tooltip>
       </div>
     </div>
@@ -28,108 +18,124 @@
 </template>
 
 <script setup lang="ts">
-  import { usePageState } from '@/stores/pageState';
-  import { useTrackState } from '@/stores/trackState';
-  import { usePlayerState } from '@/stores/playerState';
-  import { getId } from '@/utils/common';
-  import { ref, reactive, computed } from 'vue';
-  const props = defineProps({
-    modelValue: {
-      type: Number,
-      default: 30
-    }
-  });
-  const emit = defineEmits({
-    'update:modelValue': val => {
-      return val !== null;
-    }
-  });
-  const modelValue = computed({
-    get() {
-      return props.modelValue;
-    },
-    set(value) {
-      emit('update:modelValue', value);
-    }
-  });
-  const store = usePageState();
-  const trackStore = useTrackState();
-  const playerStore = usePlayerState();
-  const statePoint = computed(() => store._stepInfo.statePoint);
-  const stateLength = computed(() => store._stepInfo.stateLength);
-  const svgStyle = ref({
-    fontSize: '1.75rem',
-    padding: '0.25rem',
-    boxSizing: 'content-box'
-  });
-  const sliderProps = reactive({
-    showTooltip: false,
-    size: 'small',
-    step: 10,
-    max: 100,
-    min: 0
-  });
-  function changeScale(val: number) {
-    let newVal = modelValue.value + val;
-    if (newVal > sliderProps.max) newVal = sliderProps.max;
-    if (newVal < sliderProps.min) newVal = sliderProps.min;
-    modelValue.value = newVal;
+import { usePageState } from '@/stores/pageState';
+import { useTrackState } from '@/stores/trackState';
+import { useTrackAttrState } from '@/stores/trackAttribute';
+import { usePlayerState } from '@/stores/playerState';
+import { getId } from '@/utils/common';
+import { ref, reactive, computed } from 'vue';
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 30
   }
-  const icons = computed(() => [
-    {
-      title: '撤销',
-      disable: stateLength.value === 0 || statePoint.value === 0,
-      icon: 'UndoIcon'
-    },
-    {
-      title: '前进',
-      disable: statePoint.value === stateLength.value,
-      icon: 'RedoIcon'
-    },
-    {
-      title: '分割',
-      disable: trackStore.selectTrackItem.line === -1 && trackStore.selectTrackItem.index === -1,
-      icon: 'SplitIcon'
-    },
-    {
-      title: '删除',
-      disable: trackStore.selectTrackItem.line === -1 && trackStore.selectTrackItem.index === -1,
-      icon: 'DeleteIcon'
+});
+const emit = defineEmits({
+  'update:modelValue': val => {
+    return val !== null;
+  }
+});
+const modelValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  }
+});
+const store = usePageState();
+const trackStore = useTrackState();
+const attrStore = useTrackAttrState();
+const playerStore = usePlayerState();
+const statePoint = computed(() => store._stepInfo.statePoint);
+const stateLength = computed(() => store._stepInfo.stateLength);
+const svgStyle = ref({
+  fontSize: '1.75rem',
+  padding: '0.25rem',
+  boxSizing: 'content-box'
+});
+const sliderProps = reactive({
+  showTooltip: false,
+  size: 'small',
+  step: 10,
+  max: 100,
+  min: 0
+});
+function changeScale(val: number) {
+  let newVal = modelValue.value + val;
+  if (newVal > sliderProps.max) newVal = sliderProps.max;
+  if (newVal < sliderProps.min) newVal = sliderProps.min;
+  modelValue.value = newVal;
+}
+const icons = computed(() => [
+  {
+    title: '撤销',
+    disable: stateLength.value === 0 || statePoint.value === 0,
+    icon: 'UndoIcon'
+  },
+  {
+    title: '前进',
+    disable: statePoint.value === stateLength.value,
+    icon: 'RedoIcon'
+  },
+  {
+    title: '分割',
+    disable: trackStore.selectTrackItem.line === -1 && trackStore.selectTrackItem.index === -1,
+    icon: 'SplitIcon'
+  },
+  {
+    title: '删除',
+    disable: trackStore.selectTrackItem.line === -1 && trackStore.selectTrackItem.index === -1,
+    icon: 'DeleteIcon'
+  }
+]);
+
+function handlerIcon(item: Record<string, any>) {
+  const { icon: type, disable } = item;
+  if (disable) {
+    return;
+  }
+  if (type === 'DeleteIcon') {
+    if (trackStore.selectTrackItem.line !== -1 && trackStore.selectTrackItem.index !== -1) {
+      trackStore.removeTrack(trackStore.selectTrackItem.line, trackStore.selectTrackItem.index);
+      trackStore.selectTrackItem.line = -1;
+      trackStore.selectTrackItem.index = -1;
     }
-  ]);
+  } else if (type === 'UndoIcon') {
+    store._undo();
+  } else if (type === 'RedoIcon') {
+    store._redo();
+  } else if (type === 'SplitIcon') {
+    // 判断分割时间是否在视频内
+    let splitTime = playerStore.playStartFrame;
+    let active = trackStore.trackList[trackStore.selectTrackItem.line].list[trackStore.selectTrackItem.index];
 
-  function handlerIcon(item: Record<string, any>) {
-    const { icon: type, disable } = item;
-    if (disable) {
-      return;
-    }
-    if (type === 'DeleteIcon') {
-      if (trackStore.selectTrackItem.line !== -1 && trackStore.selectTrackItem.index !== -1) {
-        trackStore.removeTrack(trackStore.selectTrackItem.line, trackStore.selectTrackItem.index);
-        trackStore.selectTrackItem.line = -1;
-        trackStore.selectTrackItem.index = -1;
+    // Check if split time is within the active item's range
+    if (splitTime > active.start && splitTime < active.end) {
+      let copy = JSON.parse(JSON.stringify(active));
+
+      const originalEnd = active.end;
+      const originalStart = active.start;
+
+      // Update Left Item (Active)
+      active.end = splitTime;
+      active.offsetR += (originalEnd - splitTime);
+
+      // Update Right Item (Copy)
+      copy.start = splitTime;
+      copy.offsetL += (splitTime - originalStart);
+
+      copy.id = getId();
+
+      // Copy attributes
+      const originalAttrs = attrStore.trackAttrMap[active.id];
+      if (originalAttrs) {
+        const copiedAttrs = JSON.parse(JSON.stringify(originalAttrs));
+        attrStore.setTrackAttr(copy.id, copiedAttrs);
       }
-    } else if (type === 'UndoIcon') {
-      store._undo();
-    } else if (type === 'RedoIcon') {
-      store._redo();
-    } else if (type === 'SplitIcon') {
-      // 判断分割时间是否在视频内
-      let splitTime = playerStore.playStartFrame;
-      let active = trackStore.trackList[trackStore.selectTrackItem.line].list[trackStore.selectTrackItem.index];
 
-      if (splitTime > active.start && splitTime < active.end && active.type === 'video') {
-        let copy = JSON.parse(JSON.stringify(active));
-
-        active.end = splitTime;
-        active.offsetR = active.frameCount - active.end;
-
-        copy.start = splitTime;
-        copy.offsetL = splitTime - 1;
-
-        copy.id = getId();
-        trackStore.addTrack(copy, trackStore.selectTrackItem.line, false, 0);
-      }
+      trackStore.addTrack(copy, trackStore.selectTrackItem.line, false, 0);
     }
   }
+}
 </script>
